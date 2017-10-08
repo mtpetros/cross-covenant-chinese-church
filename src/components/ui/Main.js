bc2509ab1f9fda4ec4a55241aa79aae22e2ca2da
mtpetros <peter.almon@gmail.com>
2017-10-07 21:05:01 -0400

    updated nav

--- src/components/ui/Main.js
+++ src/components/ui/Main.js
@@ -3,8 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Main_components/Home'
 import About from './Main_components/About'
 
+const mainStyling = {backgroundColor: 'rgba(102,2,2,0.8)',
+                    height: '500px',
+                    width: '1900px',
+                    margin: '0 auto'};
 const Main = () => (
-    <main>
+    <main style = {mainStyling}>
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/about' component={About}/>
--- src/components/ui/Nav.js
+++ src/components/ui/Nav.js
@@ -7,24 +7,62 @@ const Nav = () => (
             <ul id="primary-left">
                 <li className="menu-bar-item">
                     <Link to='/' className='bar-link'>Home</Link>
-                    <ul className="drop-down">
-                        <li><Link to='/#location'>Location</Link></li>
-                    </ul>
                 </li>
                 <li className="menu-bar-item">
                     <Link to='/about' className='bar-link'>About</Link>
+                    <ul className="drop-down">
+                        <li><Link to='/#location'>Vision and Values</Link></li>
+                        <li><Link to='/#beliefs'>What we Believe</Link></li>
+                        <li><Link to='/#leadership'>Leadership</Link></li>
+                    </ul>
+                </li>
+                <li className="menu-bar-item">
+                    <Link to='/visit' className='bar-link'>Visit</Link>
+                    <ul className="drop-down">
+                        <li><Link to='/#time-and-location'>Time and Location</Link></li>
+                        <li><Link to='/#what-to-expect'>What to Expect</Link></li>
+                    </ul>
+                </li>
+                <li className="menu-bar-item">
+                    <Link to='/connect' className='bar-link'>Connect</Link>
+                    <ul className="drop-down">
+                        <li><Link to='/events'>Announcements and Events</Link></li>
+                        <li><Link to='/smallgroup'>Small Group</Link></li>
+                        <li><Link to='/men'>Men's Ministry</Link></li>
+                        <li><Link to='/women'>Women's Ministry</Link></li>
+                        <li><Link to='/youth'>Youth Ministry</Link></li>
+                        <li><Link to='/children'>Children's Ministry</Link></li>
+                        <li><Link to='/serving'>Serve</Link></li>
+                        <li><Link to='/membership'>Membership</Link></li>
+                        <li><Link to='/emailsignup'>Email Signup</Link></li>
+                    </ul>
                 </li>
             </ul>
-
+            <ul className="menu-logo"><li>Logo</li></ul>
             <ul id="primary-right">
                 <li className="menu-bar-item">
-                    <Link to='/' className='bar-link'>Home</Link>
+                    <Link to='/contact' className='bar-link'>Contact</Link>
                     <ul className="drop-down">
-                        <li><Link to='/#location'>Location</Link></li>
+                        <li><Link to='/#contactform'>Contact Form</Link></li>
+                        <li><Link to='/#contactinfo'>Contact and Location Information</Link></li>
+                        <li><Link to='/#servicetime'>Service Time</Link></li>
                     </ul>
                 </li>
                 <li className="menu-bar-item">
-                    <Link to='/about' className='bar-link'>About</Link>
+                    <Link to='/learn' className='bar-link'>Learn</Link>
+                    <ul className="drop-down">
+                        <li><Link to='/sermons'>Sermons</Link></li>
+                        <li><Link to='/adultseminar'>Adult Seminar</Link></li>
+                        <li><Link to='/songs'>Songs for Worship</Link></li>
+                        <li><Link to='/christianeducation'>Christian Education</Link></li>
+                    </ul>
+                </li>
+                <li className="menu-bar-item">
+                    <Link to='/missions' className='bar-link'>Missions</Link>
+                    <ul className="drop-down">
+                        <li><Link to='/#local'>Local</Link></li>
+                        <li><Link to='/#international'>International</Link></li>
+                    </ul>
                 </li>
             </ul>
         </nav>
