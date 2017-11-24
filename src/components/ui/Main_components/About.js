8575f72552542fbf793fcb49088fd7316046a382
mtpetros <peter.almon@gmail.com>
2017-11-24 14:14:48 -0500

    updated

--- src/components/ui/Main_components/About.js
+++ src/components/ui/Main_components/About.js
@@ -9,7 +9,11 @@ const About = () => (
     <div id="vision-values" className="content-area content-area-main">
         <div className="main-content-text">
             <h2>Vision and Values</h2>
-            <p>Sed rhoncus accumsan congue. Proin tempus sem tellus, ac luctus urna maximus a.</p><p>Integer vel convallis felis, et hendrerit libero.</p><p>Morbi placerat condimentum orci, eu placerat quam cursus consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin interdum orci et lectus bibendum fermentum. Praesent ut arcu non lorem consectetur vehicula non vitae magna. Morbi ut felis sem. Proin vitae tincidunt nisi. Aliquam vitae est at tortor malesuada varius.</p>
+            <br/>
+            <p>Integer non malesuada odio. Aliquam arcu nisl, tempor vitae facilisis at, vulputate luctus mi. Duis in dolor libero. Nam sodales dui consequat maximus dignissim. Sed vitae metus sit amet massa faucibus consectetur vel eu dui. Phasellus in posuere libero, id volutpat nisi.</p>
+            <br/>
+            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras sagittis diam orci. Donec eros nisi, molestie ac turpis ut, iaculis vehicula eros. Integer vitae lorem ac erat sodales cursus. Sed id scelerisque ligula. Vivamus accumsan sapien ac odio luctus consequat.</p>
         </div>
         <div className="main-content-parallax"></div>
     </div>
@@ -18,14 +22,22 @@ const About = () => (
         <div className="main-content-parallax"></div>
         <div className="main-content-text">
             <h2>What We Believe</h2>
-            <p>Sed rhoncus accumsan congue. Proin tempus sem tellus, ac luctus urna maximus a.</p><p>Integer vel convallis felis, et hendrerit libero.</p><p>Morbi placerat condimentum orci, eu placerat quam cursus consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin interdum orci et lectus bibendum fermentum. Praesent ut arcu non lorem consectetur vehicula non vitae magna. Morbi ut felis sem. Proin vitae tincidunt nisi. Aliquam vitae est at tortor malesuada varius.</p>
+            <br/>
+            <p>Integer non malesuada odio. Aliquam arcu nisl, tempor vitae facilisis at, vulputate luctus mi. Duis in dolor libero. Nam sodales dui consequat maximus dignissim. Sed vitae metus sit amet massa faucibus consectetur vel eu dui. Phasellus in posuere libero, id volutpat nisi.</p>
+            <br/>
+            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras sagittis diam orci. Donec eros nisi, molestie ac turpis ut, iaculis vehicula eros. Integer vitae lorem ac erat sodales cursus. Sed id scelerisque ligula. Vivamus accumsan sapien ac odio luctus consequat.</p>
         </div>
     </div>
 
     <div id="leadership" className="content-area content-area-main">
         <div className="main-content-text">
             <h2>Our Leaders</h2>
-            <p>Sed rhoncus accumsan congue. Proin tempus sem tellus, ac luctus urna maximus a.</p><p>Integer vel convallis felis, et hendrerit libero.</p><p>Morbi placerat condimentum orci, eu placerat quam cursus consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin interdum orci et lectus bibendum fermentum. Praesent ut arcu non lorem consectetur vehicula non vitae magna. Morbi ut felis sem. Proin vitae tincidunt nisi. Aliquam vitae est at tortor malesuada varius.</p>
+            <br/>
+            <p>Integer non malesuada odio. Aliquam arcu nisl, tempor vitae facilisis at, vulputate luctus mi. Duis in dolor libero. Nam sodales dui consequat maximus dignissim. Sed vitae metus sit amet massa faucibus consectetur vel eu dui. Phasellus in posuere libero, id volutpat nisi.</p>
+            <br/>
+            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras sagittis diam orci. Donec eros nisi, molestie ac turpis ut, iaculis vehicula eros. Integer vitae lorem ac erat sodales cursus. Sed id scelerisque ligula. Vivamus accumsan sapien ac odio luctus consequat.</p>
         </div>
         <div className="main-content-parallax"></div>
     </div>
