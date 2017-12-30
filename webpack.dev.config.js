f4bda078bdc1a99e72a351bae2359ae0c1b6c5c6
mtpetros <peter.almon@gmail.com>
2017-12-30 14:12:53 -0500

    Updated webpack.dev.config.js

---
+++ webpack.dev.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "/public/assets",
+        filename: "bundle.min.js",
+        publicPath: "/assets/"
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3000
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env':{
+              'NODE_ENV': JSON.stringify('development')
+            }
+          })
+    ],
+    module: {
+        loaders: [
+            //JS
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                include: path.join(__dirname, '/src/components'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            //CSS
+            {
+                test: /\.scss$/,
+                include: path.join(__dirname, '/src/styling'),
+                use: [{
+                    loader: "style-loader"
+                }, {
+                  loader: "css-loader" 
+                }, {
+                  loader: "sass-loader" 
+                }]
+            }
+
+        ]
+    }
+}
