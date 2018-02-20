02741b9dda4a5bfca492fdbadaf386f4eb10db32
Peter Almon <peter.almon@tma1.com>
2018-02-19 21:24:06 -0500

    Fixed parralax for main content items

--- src/components/ui/Main_components/Connect_components/Events.js
+++ src/components/ui/Main_components/Connect_components/Events.js
@@ -13,8 +13,8 @@ const eventsContent = (<div><p>The launching Service is on March 25, 10:45am.</p
 const Events = () => (
     <div id = "events-container" className="page-container">
       <Banner title="Announcements and Events" />
-      <ContentAreaMain id="announcements" title="Announcements" textPosition="text-left" content={announcementsContent} />
-      <ContentAreaMain id="events" title="Events" textPosition="text-left" content={eventsContent} />
+      <ContentAreaMain id="announcements" title="Announcements" textPosition="text-left" parallaxImage="/assets/img/times-bg.jpg" content={announcementsContent} />
+      <ContentAreaMain id="events" title="Events" textPosition="text-left" parallaxImage="/assets/img/times-bg.jpg" content={eventsContent} />
       <div className="content-area" id="content-area-contact">
           <div><p>ph: (123) 456-78910</p></div><div><p>physical address: 123 Happy Ln. Matthew, NC 28226</p></div><div><p>mailing address: 123 Happy Ln. Matthew, NC 28226</p></div>
       </div>
--- src/components/ui/Main_components/library/ContentAreaMain.js
+++ src/components/ui/Main_components/library/ContentAreaMain.js
@@ -1,13 +1,13 @@
-import styles from '../../../../styling/components/library/contentAreaMain.scss';
+import contentAreaMainStyles from '../../../../styling/components/library/contentAreaMain.scss';
 
 const ContentAreaMain = (props) => {
-    const { id, title, textPosition, content } = props;
+    const { id, title, textPosition, parallaxImage, content } = props;
     return textPosition === "text-left" ? (
-        <div id={id} className="content-area content-area-main">
-            <div className="main-content-parallax"></div>
+        <div id={id} className="content-area content-area-main" style={contentAreaMainStyles}>
+            <div className="main-content-parallax" style={{backgroundPosition: "bottom left", backgroundImage: `url(${parallaxImage})`}}></div>
             <div className="main-content-text">
                 <h2>{title}</h2>
-                <div className="text-left" style={styles}>
+                <div className="text-left">
                     {content}
                 </div>
             </div>
@@ -16,11 +16,11 @@ const ContentAreaMain = (props) => {
         <div id={id} className="content-area content-area-main">
             <div className="main-content-text">
                 <h2>{title}</h2>
-                <div className="text-right" style={styles}>
+                <div className="text-right">
                     {content}
                 </div>
             </div>
-            <div className="main-content-parallax"></div>
+            <div className="main-content-parallax" style={{backgroundPosition: "bottom left", backgroundImage: `url(${parallaxImage})`}}></div>
         </div>
     )
 }
